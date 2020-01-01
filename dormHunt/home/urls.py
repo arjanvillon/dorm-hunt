19ce7a5b3bcfa054cd4b3a454258391c4de84213
Nikki <abarcanikki@gmail.com>
2020-01-01 19:18:59 +0800

    View User Profile Layout

--- dormHunt/home/urls.py
+++ dormHunt/home/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'home'
 
 urlpatterns =[
-    path('', views.HomeView.as_view(), name='home_view'),
-    path('test/', views.TestView.as_view(), name='test_view'),
+    path('', views.HomeView.as_view(), name='view_home'),
+
+    # TODO Temporary
+    path('user/', views.UserView.as_view(), name='view_user'),
+    path('edit-user/', views.UpdateUserView.as_view(), name='update_user'),
 ]
--- dormHunt/home/views.py
+++ dormHunt/home/views.py
@@ -4,5 +4,8 @@ from django.views.generic import (TemplateView)
 class HomeView(TemplateView):
     template_name = 'home/home-tab.html'
 
-class TestView(TemplateView):
-    template_name = 'home/test.html'
+class UserView(TemplateView):
+    template_name = 'home/view-user.html'
+
+class UpdateUserView(TemplateView):
+    template_name = 'home/update-user.html'
