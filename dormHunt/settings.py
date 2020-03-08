7c8691cbce1a13502fe19880a16058170828a436
arjanlangit <heaven.arjan@gmail.com>
2020-03-08 15:35:55 +0800

    changed db to postgres and fixed back button

--- dormHunt/settings.py
+++ dormHunt/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 #error with asyncio FIXME
-# asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
 
@@ -97,8 +97,12 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dormhuntdb',
+        'USER': 'postgres',
+        'PASSWORD': '@dmin123',
+        'HOST': '127.0.0.1',
+        'PORT': '5000',
     }
 }
 
